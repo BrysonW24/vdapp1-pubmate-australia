63d2e218b5547d024d470088a13bac305b5e9fdf
Bryson Walter <Bryson_Work@hotmail.com>
2026-01-19 22:38:03 +1100

    Add README and index

---
+++ index.js
@@ -0,0 +1,8 @@
+import { AppRegistry } from 'react-native';
+import { Text, View } from 'react-native';
+
+function App() {
+  return <View><Text>Minimal Test</Text></View>;
+}
+
+AppRegistry.registerComponent('main', () => App);
